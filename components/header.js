2e39a5492b875a09029277b1b9978539c3a288a1
fehol <felix.hollmann@gmx.net>
2020-11-27 14:51:21 +0100

    Changed Port

--- components/header.js
+++ components/header.js
@@ -1,13 +1,11 @@
 // import { Fragment } from 'react';
 import Navbar from './Navigation/Navbar';
 import { css } from '@emotion/core';
-import NavbarResp from './NavigationBar/NavbarResp';
 
 export default function Header() {
   return (
     <header>
       <Navbar />
-      {/* <NavbarResp /> */}
     </header>
   );
 }

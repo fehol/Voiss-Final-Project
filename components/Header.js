7c293a0616aed7a6cf1fa09e5915c0ec08c647d6
fehol <felix.hollmann@gmx.net>
2020-11-29 08:21:15 +0100

    Components Uppercase

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+// import { Fragment } from 'react';
+import Navbar from './Navigation/Navbar';
+import { css } from '@emotion/core';
+
+export default function Header() {
+  return (
+    <header>
+      <Navbar />
+    </header>
+  );
+}
